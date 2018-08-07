ec44070ee9b4eb6fbb64dfdf8821b719a78b553e
Stanislau Shliakhtsich <stasgm@gmail.com>
2018-08-08 00:19:10 +0300

    filter refactor

--- src/app/components/AttributeBox/Attribute.tsx
+++ src/app/components/AttributeBox/Attribute.tsx
@@ -18,16 +18,4 @@ export const Attribute: React.SFC< IAttributeParams & IEntityEvent> = props => (
     <td>{props.fieldAlias}</td>
     <td>{props.sortType}</td>
   </tr>
-  // <Draggable axis="none" defaultPosition={{ x: 0, y: 0 }}>
-  //   <div className="attribute-box">
-  //     <div className="attribute-box-handle">
-  //       <div className="attribute-box-handle-title">
-  //         [ {props.expression.enityName} ] : {props.expression.fieldName} : : {props.id}
-  //       </div>
-  //       <div className="action-button" /* onClick={() => props.onClickDelete(props.attribute.id || '') }*/>
-  //         <i className="fas fa-times" />
-  //       </div>
-  //     </div>
-  //   </div>
-  // </Draggable>
 );
